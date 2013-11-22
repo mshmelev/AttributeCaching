65dc24f1a17d9103392b1e966637cfb0f8fc4154
Max Shmelev <mshmelev@gmail.com>
2013-11-22 08:03:29 -0500

    Added method to update existing cache tag dependency in CacheContext

--- AttributeCaching/CacheContext.cs
+++ AttributeCaching/CacheContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace AttributeCaching
@@ -56,12 +57,31 @@ namespace AttributeCaching
 
 
 		/// <summary>
-		/// Dependency tags for the currently executing method. Can be changed.
+		/// Dependency tags for the currently executing method. Can be changed but affects only the currently executing method.
 		/// </summary>
 		public List<string> DependencyTags
 		{
 			get;
 			private set;
+		}
+		
+
+		/// <summary>
+		/// Updates an existing dependency tag. Can be useful to append an ID value to tag
+		/// </summary>
+		/// <param name="oldTag"></param>
+		/// <param name="newTag"></param>
+		/// <returns>true, if old tag was found</returns>
+		/// <example>cacheContext.ChangeDependecyTag ("item_", "item_"+123);</example>
+		public bool ChangeDependecyTag (string oldTag, string newTag)
+		{
+			int i = DependencyTags.IndexOf (oldTag);
+			if (i == -1)
+				return false;
+
+			DependencyTags[i] = newTag;
+
+			return true;
 		}
 	}
 }
