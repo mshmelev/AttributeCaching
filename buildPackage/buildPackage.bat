7a5a96e32d0a12565d343454446fe500fbac448d
Max Shmelev <mshmelev@gmail.com>
2015-10-09 10:36:08 -0400

    Create nuget package for AttributeCaching.Redis

--- buildPackage/buildPackage.bat
+++ buildPackage/buildPackage.bat
@@ -6,4 +6,7 @@ msbuild ..\AttributeCaching.sln /t:clean;build /p:Configuration=Release
 ..\.nuget\nuget pack AttributeCaching.nuspec
 @if errorlevel 1 goto BuildFailed
 
+..\.nuget\nuget pack AttributeCaching.Redis.nuspec
+@if errorlevel 1 goto BuildFailed
+
 :BuildFailed
