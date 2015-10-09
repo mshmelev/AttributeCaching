0efcd3b9fd9f442e5dd83517f42128a8199b3c19
Max Shmelev <mshmelev@gmail.com>
2015-10-09 10:13:52 -0400

    Create nuget package

--- AttributeCaching/CacheAdapters/CacheItemWrapper.cs
+++ AttributeCaching/CacheAdapters/CacheItemWrapper.cs
@@ -5,6 +5,9 @@
 	/// </summary>
 	public sealed class CacheItemWrapper
 	{
+        /// <summary>
+        /// Cached value
+        /// </summary>
 		public object Value { get; set; }
 	}
 }
---
+++ buildPackage/buildPackage.bat
@@ -0,0 +1,9 @@
+@del AttributeCaching*.nupkg
+
+msbuild ..\AttributeCaching.sln /t:clean;build /p:Configuration=Release
+@if errorlevel 1 goto BuildFailed
+
+..\.nuget\nuget pack AttributeCaching.nuspec
+@if errorlevel 1 goto BuildFailed
+
+:BuildFailed
